4225d63be542bb8dc801c4e09f00f5c448bccf89
“technologeve” <eve.nicole@icloud.com>
2024-05-30 16:05:12 +0100

    Add comments

--- calorfree/scripts/content.js
+++ calorfree/scripts/content.js
@@ -1,8 +1,9 @@
 
-
+// Define the Reguldar Expression string
 const wordMatchRegExp = /([0-9]+) ?(calories?|cal|kcals?)|(calories?|cal|kcals?):? ?([0-9]+)/g; // regular expression
-// const words = text.matchAll(wordMatchRegExp);
 
-
+// Make this a RegExp() object
 const regex = new RegExp(wordMatchRegExp, 'gi'); // 'gi' for case-insensitive replacement
+
+// Replace all instances of text with said regular expression
 document.body.innerHTML = document.body.innerHTML.replaceAll(regex, "");
