e2f4391525eb435c9dfacd1ea2de4a4d8493214a
“technologeve” <eve.nicole@icloud.com>
2024-05-31 15:19:05 +0100

    Remove nutrifox.com nutrition labels

--- calorfree/scripts/content.js
+++ calorfree/scripts/content.js
@@ -1,9 +1,14 @@
 
 // Define the Reguldar Expression string
-const wordMatchRegExp = /([0-9]+) ?(calories?|cal|kcals?)|(calories?|cal|kcals?):? ?([0-9]+)/g; // regular expression
+const wordMatchRegExp = /([0-9]+) ?(calorie|cal|kcal)s?( per serving)?|(calories?|cal|kcals?)( per serving)?:? ?([0-9]+)/g; // regular expression
 
 // Make this a RegExp() object
 const regex = new RegExp(wordMatchRegExp, 'gi'); // 'gi' for case-insensitive replacement
 
 // Replace all instances of text with said regular expression
 document.body.innerHTML = document.body.innerHTML.replaceAll(regex, "");
+
+// Remove nutritional informayion from Nutrifox plugin
+const wordMatchRegExpNutrifox = /https:\/\/nutrifox.com\/embed\/label\/[0-9]+/g; // regular expression
+const regexNutri = new RegExp(wordMatchRegExpNutrifox, 'gi'); // 'gi' for case-insensitive replacement
+document.body.innerHTML = document.body.innerHTML.replaceAll(regexNutri, "");
