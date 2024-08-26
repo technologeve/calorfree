a12e0ee53521857730633f3ef482b5d7cee94020
“technologeve” <eve.nicole@icloud.com>
2024-08-26 15:57:15 +0100

    First working unit test

--- test-calorfree/content.test.js
+++ test-calorfree/content.test.js
@@ -6,9 +6,16 @@ beforeEach(() => {
         Object.defineProperty(global, 'document.body.html', {});
         document.body.innerHTML = '<div id="app">Calories: 20</div>';
         
-        Object.defineProperty(global, 'chrome.storage.', {});
-        // document.body.innerHTML = '<div id="app">Calories: 20</div>';
+        chrome.storage.sync.clear();
+        // chrome.stroage.set({"hideAdditionalInfo": false});
+        chrome.storage.sync.set({hideAdditionalInfo: false, key2: 'value2' }, () => {
+          console.log('sync storage initialized');
+        });
       });
+    
+afterAll(() => {
+  document.body.innerHTML = "";
+})
 
 test("Removes 'Calories: 20'", () => {
     const generalCalorieRegExp = /([0-9]+) ?(calorie|cal|kcal)s?( per serving)?|(calories?|cal|kcals?)( per serving)?:? ?([0-9]+)(.[0-9]+)? ?(\\([0-9]+%\\))?/g;
