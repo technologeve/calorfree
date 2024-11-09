e262cba3b7a1e395ff2de990916a54c9a1bdde5a
“technologeve” <eve.nicole@icloud.com>
2024-11-09 23:06:56 +0000

    WIP, basicRegexReplacement test expansion

--- test-calorfree/content.test.js
+++ test-calorfree/content.test.js
@@ -1,25 +1,57 @@
 
 const basicRegexReplacement = require("../calorfree/scripts/content");
+// const replaceInfoSplitByChevrons = require("../calorfree/scripts/content");
 
-beforeEach(() => {
-        // Set up a mock DOM environment before each test
-        Object.defineProperty(global, 'document.body.html', {});
-        document.body.innerHTML = '<div id="app">Calories: 20</div>';
-        
-        chrome.storage.sync.clear();
-        // chrome.stroage.set({"hideAdditionalInfo": false});
-        chrome.storage.sync.set({hideAdditionalInfo: false, key2: 'value2' }, () => {
-          console.log('sync storage initialized');
-        });
-      });
+function setupDocumentBody(textToSetup){
+  // Set up a mock DOM environment before each test
+  Object.defineProperty(global, 'document.body.html', {});
+  document.body.innerHTML = textToSetup;
+  
+  chrome.storage.sync.clear();
+  // chrome.stroage.set({"hideAdditionalInfo": false});
+  chrome.storage.sync.set({hideAdditionalInfo: false, key2: 'value2' }, () => {
+    console.log('sync storage initialized');
+  });
+}
     
 afterAll(() => {
   document.body.innerHTML = "";
 })
 
-test("Removes 'Calories: 20'", () => {
-    const generalCalorieRegExp = /([0-9]+) ?(calorie|cal|kcal)s?( per serving)?|(calories?|cal|kcals?)( per serving)?:? ?([0-9]+)(.[0-9]+)? ?(\\([0-9]+%\\))?/g;
-    basicRegexReplacement(generalCalorieRegExp)
-    expect(document.body.innerHTML).toBe("<div id=\"app\"></div>");
-    
+// Test basicRegexReplacement
+test("replaces regex where present", () => {
+  setupDocumentBody('Test Text')
+  const generalCalorieRegExp = /Test /g;
+  basicRegexReplacement(generalCalorieRegExp)
+  expect(document.body.innerHTML).toBe("Text");
 });
+
+test("doesn't replace non present regex", () => {
+  setupDocumentBody('More Text')
+  const generalCalorieRegExp = /Test/g;
+  basicRegexReplacement(generalCalorieRegExp)
+  expect(document.body.innerHTML).toBe("More Text");
+});
+
+test("Removes 'Calories: 20'", () => {
+  setupDocumentBody('<div id="app">Calories: 20</div>')
+  const generalCalorieRegExp = /([0-9]+) ?(calorie|cal|kcal)s?( per serving)?|(calories?|cal|kcals?)( per serving)?:? ?([0-9]+)(.[0-9]+)? ?(\\([0-9]+%\\))?/g;
+  basicRegexReplacement(generalCalorieRegExp)
+  expect(document.body.innerHTML).toBe("<div id=\"app\"></div>");
+  
+});
+
+// Test replaceInfoSplitByChevrons
+// test("two chevrons", () => {
+//   setupDocumentBody('Before<test><text>After')
+//   const generalCalorieRegExp = //g;
+//   replaceInfoSplitByChevrons(generalCalorieRegExp)
+//   expect(document.body.innerHTML).toBe("Before<>After");
+// })
+
+// test("Removes 'Calories:{one}{two}20'", () => {
+//   const generalCalorieRegExp = /([0-9]+) ?(calorie|cal|kcal)s?( per serving)?|(calories?|cal|kcals?)( per serving)?:? ?([0-9]+)(.[0-9]+)? ?(\\([0-9]+%\\))?/g;
+//   basicRegexReplacement(generalCalorieRegExp)
+//   expect(document.body.innerHTML).toBe("<div id=\"app\"></div>");
+  
+// });
